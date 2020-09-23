cb63352132881fccf55bd3e74a2a72e02b1ed4b3
KrisWaw <44495365+KrisWaw@users.noreply.github.com>
2020-09-23 20:54:45 +0200

    add CRUDController.php and InvalidOrNoDataResponse.php

---
+++ app/controller/CRUDController.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace controller;
+
+
+abstract class CRUDController {
+
+    protected $service;
+    protected string $requestMethod;
+    protected ?int $id;
+
+    /**
+     * CRUDController constructor
+     * @param string $requestMethod
+     * @param int|null $id
+     */
+    public function __construct(string $requestMethod, ?int $id) {
+        $this->requestMethod = $requestMethod;
+        $this->id = $id;
+    }
+
+    abstract public function processRequest();
+
+    abstract protected function findAllRequest();
+
+    abstract protected function findOneRequest();
+
+    abstract protected function createRequest();
+
+    abstract protected function updateRequest();
+
+    abstract protected function deleteRequest();
+
+    abstract protected function validateInput();
+}
---
+++ app/controller/InvalidOrNoDataResponse.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace controller;
+
+
+trait InvalidOrNoDataResponse {
+
+    private function notFoundResponse() {
+        $response['status_code_header'] = 'HTTP/1.1 422 Unprocessable Entity';
+        $response['body'] = json_encode([
+            'error' => 'Invalid input'
+        ]);
+        return $response;
+    }
+
+    private function unprocessableEntityResponse() {
+        $response['status_code_header'] = 'HTTP/1.1 404 Not Found';
+        $response['body'] = null;
+        return $response;
+    }
+}
