2103baf99a0346881822c6ced54fa434fdc674af
KrisWaw <44495365+KrisWaw@users.noreply.github.com>
2020-10-01 01:32:33 +0200

    refactor InvalidOrNoDataResponse.php

--- app/controller/InvalidOrNoDataResponse.php
+++ app/controller/InvalidOrNoDataResponse.php
@@ -8,15 +8,15 @@ trait InvalidOrNoDataResponse {
 
     private function notFoundResponse() {
         $response['status_code_header'] = 'HTTP/1.1 404 Not Found';
-        $response['body'] = json_encode([
-            'error' => 'Invalid input'
-        ]);
+        $response['body'] = null;
         return $response;
     }
 
     private function unprocessableEntityResponse() {
         $response['status_code_header'] = 'HTTP/1.1 422 Unprocessable Entity';
-        $response['body'] = null;
+        $response['body'] = json_encode([
+            'error' => 'Invalid input'
+        ]);
         return $response;
     }
 }
